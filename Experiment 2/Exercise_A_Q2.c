cae4da3182ad182238444211e99db58b1c09dc9d
Harsh Deshmukh <dnitin111970@gmail.com>
2026-08-03 12:58:28 +0530

    Create Exercise_A_Q2.c

---
+++ Experiment 2/Exercise_A_Q2.c
@@ -0,0 +1,22 @@
+// Code by Harsh Deshmukh SY-CSE B
+// Write a program to count set bits
+
+#include <stdio.h>
+
+int main() {
+    int num, count = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while (num > 0) {
+        if (num & 1)
+            count++;
+
+        num = num >> 1;
+    }
+
+    printf("Number of set bits = %d\n", count);
+
+    return 0;
+}
