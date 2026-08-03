6d57d8064f4e020ab978bcf9155497ad68635914
Harsh Deshmukh <dnitin111970@gmail.com>
2026-08-03 13:14:00 +0530

    Create Exercise_A_Q4.c

---
+++ Experiment 3/Exercise_A_Q4.c
@@ -0,0 +1,30 @@
+// Code by Harsh Deshmukh SY-CSE B
+// Find the smallest element using Linear Search
+
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter the array elements:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int smallest = arr[0];
+
+    for (i = 1; i < n; i++) {
+        if (arr[i] < smallest) {
+            smallest = arr[i];
+        }
+    }
+
+    printf("Smallest element = %d\n", smallest);
+
+    return 0;
+}
