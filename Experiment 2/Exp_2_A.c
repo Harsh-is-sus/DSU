6d3218a8487f03f10bd1e4e2419b4a51cac2bb4f
Harsh Deshmukh <dnitin111970@gmail.com>
2026-08-03 12:55:54 +0530

    Create Exp_2_A.c

---
+++ Experiment 2/Exp_2_A.c
@@ -0,0 +1,21 @@
+// Code by Harsh Deshmukh SY-CSE B
+// Write a 'C' Program for Checking a Bit at Nth Position (Set or Clear)
+
+#include <stdio.h>
+
+int main() {
+    int num, n;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Enter the bit position (0-based): ");
+    scanf("%d", &n);
+
+    if (num & (1 << n))
+        printf("Bit at position %d is SET (1).\n", n);
+    else
+        printf("Bit at position %d is CLEAR (0).\n", n);
+
+    return 0;
+}
