26692d323e2e3002f973c2421c67dba3bd2138c1
Harsh Deshmukh <dnitin111970@gmail.com>
2026-08-03 13:11:14 +0530

    Create Exercise_A_Q3.c

---
+++ Experiment 3/Exercise_A_Q3.c
@@ -0,0 +1,30 @@
+// Code by Harsh Deshmukh SY-CSE B
+// Find the largest element using Linear Search
+
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter the array elements:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int largest = arr[0];
+
+    for (i = 1; i < n; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+
+    printf("Largest element = %d\n", largest);
+
+    return 0;
+}
