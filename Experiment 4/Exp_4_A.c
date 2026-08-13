ff7adbde1cf7a44265e44111e044af9721cb8c3a
Harsh Deshmukh <dnitin111970@gmail.com>
2026-08-13 09:43:51 +0530

    Implement array sorting before searching for an element
    Added sorting functionality to the array before searching.

--- Experiment 4/Exp_4_A.c
+++ Experiment 4/Exp_4_A.c
@@ -5,21 +5,42 @@
 
 int main()
 {
-    int a[100], n, i, key;
+    int a[100], n, i, j, temp, key;
     int low, high, mid, found = 0;
 
     printf("Enter number of elements: ");
     scanf("%d", &n);
 
-    printf("Enter elements in ascending order:\n");
+    printf("Enter elements:\n");
     for(i = 0; i < n; i++)
     {
         scanf("%d", &a[i]);
     }
 
-    printf("Enter element to search: ");
+   
+    for(i = 0; i < n - 1; i++)
+    {
+        for(j = i + 1; j < n; j++)
+        {
+            if(a[i] > a[j])
+            {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+
+    printf("Array in ascending order:\n");
+    for(i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    printf("\nEnter element to search: ");
     scanf("%d", &key);
 
+   
     low = 0;
     high = n - 1;
 
